3f42c6b1e625387133bde018920b10be798f9b8a
Naol Daba <115622877+Naoldaba@users.noreply.github.com>
2024-02-20 19:16:03 +0300

    Add solution for Decode String

---
+++ progress_shit/leetcode/decode-string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack=[]
+        for char in s:
+            if char!=']':
+                stack.append(char)
+            else:
+                temp=''
+                while stack and stack[-1]!='[':
+                    temp=stack.pop()+temp
+                stack.pop()
+
+                num=''
+                while stack and stack[-1].isdigit():
+                    num=stack.pop()+num
+                
+                stack.append(int(num)*temp)
+        return ''.join(stack)
